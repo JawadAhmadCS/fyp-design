024f5137e0bd146522f78d7581df7561bfbdc067
JawadAhmadCS <mjawadahmad41@gmail.com>
2026-03-29 13:48:50 +0500

    just html/css/js

--- app/src/components/BrandWordmark.tsx
+++
@@ -1,7 +0,0 @@
-﻿interface BrandWordmarkProps {
-  compact?: boolean;
-}
-
-export default function BrandWordmark({ compact = false }: BrandWordmarkProps) {
-  return <span className={compact ? "brand-wordmark compact" : "brand-wordmark"}>Ovixe</span>;
-}
